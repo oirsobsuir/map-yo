fc282a09d4e49c783344550bbd2125e32df2eab6
Roman Lyakh <ctarewer@gmail.com>
2024-02-28 16:39:22 +0300

    feat: add logic for double click style: add class to selector

--- js/main.js
+++ js/main.js
@@ -8,34 +8,48 @@ async function fetchData() {
         console.error('Ошибка загрузки данных JSON:', error);
     }
 
-    const{data: country, obls} = jsonData;
+    const {data: country, obls} = jsonData;
 
     let current = country;
 
-
+    let clickCount = 0;
     let hoverElement = document.querySelectorAll("g");
     let title = document.getElementById("title");
     let box = document.getElementsByClassName('box')[0];
     let values = document.getElementsByClassName('value');
     let numbers = document.getElementsByClassName('number');
+    let checky
 
-    for(let i = 0; i < numbers.length; i++){
-        numbers[i].textContent = current['n'+ (i + 1)]
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i].textContent = current['n' + (i + 1)]
     }
 
-
     for (let item of hoverElement) {
-        item.addEventListener("mouseover", function() {
-            console.log(obls[item.id])
+        item.addEventListener("mouseover", function () {
             box.style.display = 'block'
             item.id === '6' ? title.textContent = 'город ' + obls[item.id].name : title.textContent = obls[item.id].name + ' область'
-            for(let i = 0; i < values.length; i++){
-                values[i].textContent = obls[item.id]['n'+ (i + 1)]
+            for (let i = 0; i < values.length; i++) {
+                values[i].textContent = obls[item.id]['n' + (i + 1)]
             }
         });
 
-        item.addEventListener("mouseout", function() {
+        item.addEventListener("mouseout", function () {
             box.style.display = 'none'
+        });
+
+        item.addEventListener("click", function (event) {
+            if (item.contains(event.target) && checky === item) {
+                clickCount++;
+                checky = item
+                if (clickCount === 2) {
+
+
+                    clickCount = 0;
+                }
+            } else {
+                checky = item
+                clickCount = 1;
+            }
         });
     }
 
