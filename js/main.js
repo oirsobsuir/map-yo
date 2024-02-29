765e6d7e54743a2f51c0b4261a67863382e55a82
Roman Lyakh <ctarewer@gmail.com>
2024-02-29 16:52:17 +0300

    refactor: and control on elements and add id in Minsk map

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let jsonData;
+let back_button = document.getElementsByClassName('back')[0];
 
 async function fetchData() {
     try {
@@ -10,49 +11,67 @@ async function fetchData() {
 
     const {data: country, obls} = jsonData;
 
-    let current = country;
-
     let clickCount = 0;
-    let hoverElement = document.querySelectorAll("g");
+    let hoverElement = document.querySelectorAll(".bel_map g");
     let title = document.getElementById("title");
     let box = document.getElementsByClassName('box')[0];
     let values = document.getElementsByClassName('value');
     let numbers = document.getElementsByClassName('number');
-    let checky
-
-    for (let i = 0; i < numbers.length; i++) {
-        numbers[i].textContent = current['n' + (i + 1)]
-    }
-
-    for (let item of hoverElement) {
-        item.addEventListener("mouseover", function () {
-            box.style.display = 'block'
-            item.id === '6' ? title.textContent = 'город ' + obls[item.id].name : title.textContent = obls[item.id].name + ' область'
-            for (let i = 0; i < values.length; i++) {
-                values[i].textContent = obls[item.id]['n' + (i + 1)]
-            }
-        });
-
-        item.addEventListener("mouseout", function () {
-            box.style.display = 'none'
-        });
-
-        item.addEventListener("click", function (event) {
-            if (item.contains(event.target) && checky === item) {
-                clickCount++;
-                checky = item
-                if (clickCount === 2) {
+    let minsk = document.getElementsByClassName('minsk')[0];
+    let rb = document.getElementsByClassName('rb')[0];
 
 
-                    clickCount = 0;
+    function showSettlementInfo (currentMainObj, blocks, data) {
+        let checky
+        back_button.style.display =  currentMainObj === country ? 'none' : 'block'
+
+
+        for (let i = 0; i < numbers.length; i++) {
+            numbers[i].textContent = currentMainObj['n' + (i + 1)]
+        }
+
+        for (let item of blocks) {
+            item.addEventListener("mouseover", function () {
+                box.style.display = 'block'
+                item.id === '6' ? title.textContent = 'город ' + data[item.id].name : title.textContent = data[item.id].name + ' область'
+                for (let i = 0; i < values.length; i++) {
+                    values[i].textContent = data[item.id]['n' + (i + 1)]
                 }
-            } else {
-                checky = item
-                clickCount = 1;
-            }
-        });
-    }
+            });
 
+            item.addEventListener("mouseout", function () {
+                box.style.display = 'none'
+            });
+            console.log(currentMainObj.id)
+            currentMainObj.id === 'r1' && item.addEventListener("click", function (event) {
+                if (item.contains(event.target) && checky === item) {
+                    clickCount++;
+                    if (clickCount === 2) {
+                        currentMainObj = data[item.id]
+                        data = currentMainObj.cities;
+                        item.parentNode.classList.remove("bel_map");
+                        minsk.classList.add("bel_map");
+                        let newHoverElement = document.querySelectorAll(".bel_map g");
+                        showSettlementInfo(currentMainObj, newHoverElement, data);
+                        clickCount = 0;
+                    }
+                } else {
+                    checky = item
+                    clickCount = 1;
+                }
+
+            });
+        }
+    }
+    back_button.addEventListener("click",  function() {
+        minsk.classList.remove("bel_map");
+        rb.classList.add("bel_map");
+        showSettlementInfo(country, hoverElement, obls)
+    })
+
+    showSettlementInfo(country, hoverElement, obls)
 }
+
+
 
 fetchData();
