de7bacdb877798263493c12e6fdccdcb0b5bd081
Roman Lyakh <ctarewer@gmail.com>
2024-03-02 21:58:20 +0300

    fix: solve problem with visible box

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 let jsonData;
-let back_button = document.getElementsByClassName('back')[0];
 
 async function fetchData() {
     try {
@@ -19,9 +18,10 @@ async function fetchData() {
     let numbers = document.getElementsByClassName('number');
     let rb = document.getElementsByClassName('rb')[0];
     let mainTitle = document.querySelector('.titles h2')
+    let back_button = document.getElementsByClassName('back')[0];
 
-    function showSettlementInfo (currentMainObj, blocks, data) {
-        let checky
+    function showValues (currentMainObj) {
+
         back_button.style.display =  currentMainObj === country ? 'none' : 'block'
 
         if(currentMainObj === country) {
@@ -35,6 +35,12 @@ async function fetchData() {
         for (let i = 0; i < numbers.length; i++) {
             numbers[i].textContent = currentMainObj['n' + (i + 1)]
         }
+    }
+
+    function showSettlementInfo (currentMainObj, blocks, data) {
+        let checky
+
+        showValues(currentMainObj)
 
         for (let item of blocks) {
             item.addEventListener("mouseover", function () {
@@ -67,10 +73,10 @@ async function fetchData() {
                 if (item.contains(event.target) && checky === item) {
                     clickCount++;
                     if (clickCount === 2) {
-                        item.parentNode.classList.remove("bel_map");
+                        document.querySelector(`.bel_map`).classList.remove("bel_map");
                         document.querySelectorAll(`.map svg`)[+item.id + 1].classList.add("bel_map")
                         let newHoverElement = document.querySelectorAll(".bel_map g");
-                        item.removeEventListener("click", clickHandler, false)
+                        box.style.display = 'none';
                         showSettlementInfo(data[item.id], newHoverElement, data[item.id].cities);
                         clickCount = 0;
                     }
@@ -78,14 +84,14 @@ async function fetchData() {
                     checky = item
                     clickCount = 1;
                 }
-                
             });
         }
     }
+
     back_button.addEventListener("click",  function() {
         document.querySelector(`.bel_map`).classList.remove("bel_map");
         rb.classList.add("bel_map");
-        showSettlementInfo(country, hoverElement, obls)
+        showValues(country)
     })
 
     showSettlementInfo(country, hoverElement, obls)
